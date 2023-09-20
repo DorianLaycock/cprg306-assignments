bcae2a33da922e4e49285c5aed24cfd37603577d
DorianLaycock <119632701+DorianLaycock@users.noreply.github.com>
2023-09-20 13:03:38 -0600

    Created page 3
    Will add the appropriate elements later.

---
+++ app/week2/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+    <main>
+        <h1>Welcome to week 2.</h1>
+        <p>This is week 2. Behold.</p>
+    </main>
+    );
+}
---
+++ app/week3/page.js
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+    <main>
+      <h1 className="text-4xl">This is week 3</h1>
+      <p>I will work on the lab</p>
+      <p>And get it done in time for the deadline</p>
+      <a href ="/week2">Week 2</a>
+    </main>
+    );
+  }
