32210a757c8534194afb52a403fe72af23b03250
DorianLaycock <119632701+DorianLaycock@users.noreply.github.com>
2023-09-29 22:24:00 -0600

    Added files for week 4

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,10 +1,9 @@
 export default function Item ({name, quantity, category}){
     return (
-        <ul>
+        <ul className= "border border-red-700">
             <li className="font-bold">Item: {name}</li>
             <li>Quantity:{quantity}</li>
             <li>Category:{category}</li>
-            
         </ul>
     )
 }
---
+++ app/week4/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+  <main>
+    <h1>Week 4</h1>
+    <Link href ="/">Home Page</Link>
+  </main>
+  );
+}
