dcbf447b403aaca2097520eda4028aeec034e875
DorianLaycock <119632701+DorianLaycock@users.noreply.github.com>
2023-10-09 22:49:31 -0600

    Gave the homepage a minor makeover

--- app/page.js
+++ app/page.js
@@ -2,14 +2,18 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-  <main>
-    <h1 className="text-4xl">CPRG 306 - Web Development 2 - Assignments</h1>
-    <p>Name: Dorian Laycock</p>
-    <p>Course Section: CPRG 306 A</p>
-    <Link href ="/week2" >Week 2</Link>
-    <Link href ="/week3" >Week 3</Link>
-    <Link href ="/week4" >Week 4</Link>
-    <Link href ="/week5" >Week 5</Link>
+  <main class="flex justify-center items-center w-screen">
+    <div class ="text-center">
+      <h1 className="text-4xl">CPRG 306 - Web Development 2 - Assignments</h1>
+      <p>Name: Dorian Laycock</p>
+      <p>Course Section: CPRG 306 A</p>
+      <div class="pt-5">
+        <Link class = "block text-red-600" href ="/week2" >• Week 2</Link>
+        <Link class = "block text-red-600" href ="/week3" >• Week 3</Link>
+        <Link class = "block text-red-600" href ="/week4" >• Week 4</Link>
+        <Link class = "block text-red-600" href ="/week5" >• Week 5</Link>
+      </div>
+    </div>
   </main>
   );
 }
