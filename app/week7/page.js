b34cd9a8a773c4c43a3824b212763ca3fc976dce
DorianLaycock <119632701+DorianLaycock@users.noreply.github.com>
2023-10-25 10:42:28 -0600

    Basis for week 7 assignment
    Fixed bugs from week 6, will integrate API next.

--- app/week7/page.js
+++ app/week7/page.js
@@ -1,59 +1,34 @@
 "use client";
 
-import {useState, useEffect} from "react"; //research this more.
+import {useState} from "react";
+import Itemlist from "./itemlist.js";
+import Itemform from "./itemform.js";
+import Item from "./item.js";
 
-async function fetchRandomDog(){
-    try{
-        const response = await fetch("https://dog.ceo/api/breeds/image/random");
-        const data = await response.json();
-        return data.message;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function fetchDogBreeds(){
-    try{
-        const response = await fetch("https://dog.ceo/api/breeds/list/all");
-        const data = await response.json();
-        return data.message;
-    } catch (error){
-        console.error(error);
-    }
-}
+const itemsData = [
+  {name:"Wine", quantity:4, category:"produce"},
+  {name:"Spaghetti", quantity:7, category:"produce"},
+  {name:"Mozarella", quantity:3, category:"produce"},
+];
 
 export default function Page() {
-    const [dog, setDog] = useState("");
-    const [breeds, setBreeds] = useState([]);
 
-    async function loadRandomDog() {
-        const randomDog = await fetchRandomDog();
-        setDog(randomDog);
-    }
+  const [items, setItems] = useState(itemsData);
 
-    async function loadDogBreeds(){
-        const breeds = await fetchDogBreeds();
-        setBreeds(breeds);
-    }
+  function handleSubmit(item) {
+    setItems([...items, item]);
+  }
 
-    useEffect(() => {
-        loadRandomDog()
-        loadDogBreeds();
-    }, []);
+  function handleDelete(name){
+    setItems(items.filter((item) => item.name !== name));
+  }
 
-    return (
-        <main>
-            <h1>Dogs!</h1>
-            <div>
-                <select name = "breeds">
-                    {Object.keys(breeds).map((breed) => (
-                        <option key={breed}>{breed}</option>
-                    ))}
-                </select>
-            </div>
-            <div>
-                <img src={dog}/>
-            </div>
-        </main>
-    )
+  return (
+  <main class="flex justify-center items-center w-screen">
+    <div class="text-left">
+      <Itemform onSubmit ={(item) => handleSubmit(item)} />
+      <Itemlist items ={items} onDelete = {handleDelete} />
+    </div>
+  </main>
+  );
 }
