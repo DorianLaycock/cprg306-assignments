f9e795d27b6d6d4d93b0aca218a6d8974f35efca
DorianLaycock <119632701+DorianLaycock@users.noreply.github.com>
2023-11-20 09:40:26 -0700

    Made some changes for week 10
    Will continue when I have time.

---
+++ app/week10/_utils/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvW4ICAqSTMEt42jU-Co5BETQ9GQhyLQQ",
+  authDomain: "cprg306-assignments-abef3.firebaseapp.com",
+  projectId: "cprg306-assignments-abef3",
+  storageBucket: "cprg306-assignments-abef3.appspot.com",
+  messagingSenderId: "151016410017",
+  appId: "1:151016410017:web:a979f43c9e0780c22119e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
---
+++ app/week10/page.js
@@ -0,0 +1,68 @@
+//Got as far as part 5 for week 8 assignment. Continue working later.
+
+"use client";
+
+import Link from "next/link";
+import {useState, useEffect} from "react";
+import Itemlist from "./itemlist.js";
+import Itemform from "./itemform.js";
+import Item from "./item.js";
+
+const itemsData = [
+  {name:"Wine", quantity:4, category:"produce"},
+  {name:"Spaghetti", quantity:7, category:"produce"},
+  {name:"Mozarella", quantity:3, category:"produce"},
+];
+
+async function fetchRecipe(){
+  try {
+    response = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}");
+    const data = await response.json();
+    return data.message;
+  } catch (error){
+    console.error(error);
+  }
+}
+
+export default function Page() {
+
+  const [items, setItems] = useState(itemsData);
+  const [recipe, setRecipe] = useState([]);
+
+  async function loadRecipes(){
+    const recipes = await fetchRecipe();
+    setRecipe(recipes);
+  }
+
+  useEffect(() => {
+    loadRecipes();
+  }, []);
+
+  function handleSubmit(item){
+    setItems([...items, item]);
+  }
+
+  function handleDelete(name){
+    setItems(items.filter((item) => item.name !== name));
+  } 
+
+  return (
+  <main>
+    <div className="flex flex-col justify-center items-center w-screen">
+      <h1 className="text-4xl">Week 7</h1>
+      <Link class = "block text-red-600 pb-2" href="/">• Homepage</Link>
+    </div>
+    <div className="flex flex-row border border-white">
+      <div className="flex basis-1/3 w-150">
+        <Itemform onSubmit ={(item) => handleSubmit(item)} />
+      </div>
+      <div className="flex basis-1/3 justify-center border border-white"> 
+        Meal Ideas {/* https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient} API will be integrated with this section */}
+      </div>
+    </div>
+    <div>
+      <Itemlist items ={items} onDelete = {handleDelete} />
+    </div>
+  </main>
+  );
+}
